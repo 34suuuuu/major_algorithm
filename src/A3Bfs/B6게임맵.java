2d0d223882909a9ff8b030d9c257723bea90df63
suhyunpark <psh7492@gmail.com>
2024-06-26 10:26:44 +0900

    프로그래머스) 게임 맵 최단거리

--- src/A3Bfs/B6게임맵.java
+++ src/A3Bfs/B6게임맵.java
@@ -6,7 +6,6 @@ import java.util.Queue;
 public class B6게임맵 {
 	static int[] dx = {0, 0, 1, -1};
 	static int[] dy = {1, -1, 0, 0};
-	static boolean[][] visited;
 	static int n, m;
 
 	public static void main(String[] args) {
@@ -14,21 +13,19 @@ public class B6게임맵 {
 		n = maps.length;
 		m = maps[0].length;
 
-		visited = new boolean[n][m];
 		System.out.println(bfs(maps));
 	}
 
 	static int bfs(int[][] maps) {
 		Queue<int[]> que = new LinkedList<>();
 		que.add(new int[] {0, 0, 1});
-		visited[0][0] = true;
+		maps[0][0] = -1;	// 방문 배열을 삭제하고 값을 수정하는 방식으로
 
 		while (!que.isEmpty()) {
 			int[] cur = que.poll();
 			int curX = cur[0];
 			int curY = cur[1];
 			int move = cur[2];
-			visited[curX][curY] = true;
 
 			if (curX == n - 1 && curY == m - 1) {
 				// 종료 조건
@@ -41,8 +38,8 @@ public class B6게임맵 {
 
 				if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;	// 이동 지점이 maps안에 있는지 확인
 
-				if (!visited[nx][ny] && maps[nx][ny] == 1) {// 방문하지않았고, 벽이 아니라면 방문
-					visited[nx][ny] = true;
+				if (maps[nx][ny] == 1) {// 방문하지않았고, 벽이 아니라면 방문
+					maps[nx][ny] = -1;
 					que.add(new int[] {nx, ny, move + 1});
 				}
 			}
