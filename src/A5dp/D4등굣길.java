e2e3e347eda6939bec027a36d002f17d5dfdf022
suhyunpark <psh7492@gmail.com>
2024-06-27 09:12:29 +0900

    프로그래머스) 등굣길

---
+++ src/A5dp/D4등굣길.java
@@ -0,0 +1,34 @@
+package A5dp;
+
+public class D4등굣길 {
+	public static void main(String[] args) {
+		int m = 4;
+		int n = 3;
+		int[][] puddles = {{2, 2}};
+
+		int[][] dp = new int[n + 1][m + 1];
+		final int div = 1000000007;
+
+		for (int i = 0; i < puddles.length; i++) {
+			dp[puddles[i][1]][puddles[i][0]] = -1;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (dp[i][j] == -1) {
+					dp[i][j] = 0;
+					continue;
+				}
+
+				if (!(i == 1 && j == 1)) {
+					dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % div;
+				} else {
+					dp[i][j] = 1;
+				}
+			}
+		}
+
+		System.out.println(dp[n][m]);
+	}
+
+}
